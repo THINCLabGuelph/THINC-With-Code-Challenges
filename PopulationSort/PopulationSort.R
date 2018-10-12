760d9e4794706c2a1f485ec8caf249222762349d
Adam Doan <adam.c.doan@gmail.com>
2018-10-12 09:52:51 -0400

    Now with challenge for week 2.

---
+++ PopulationSort/PopulationSort.R
@@ -0,0 +1,3 @@
+# Challenge: Using the gapminder dataset (contained in gapminder_data.csv)
+# Create a new data frame that contains the country names and the 2007 population, sorted by population in descending order.
+# Print out the top 5 countries by population
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Weekly code challenges for the [THINC With Code](https://www.uoguelph.ca/arts/dhguelph/thinc) group. 
 
 Week 1: NameReverser
+Week 2: PopulationSort
